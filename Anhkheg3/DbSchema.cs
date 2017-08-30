4638181aaa13b40e34317819b47a640b2d41d105
Patrick Luhmann <patrick.luhmann@gmail.com>
2017-08-30 14:05:24 -0700

    Add model via DbSchema.cs
    The first version of the database schema is defined by the model
    in DbSchema.cs. The migration command has not yet been executed
    so there might be problems with it.
    
    The EF Core packages are also added in this commit.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ Anhkheg3/DbSchema.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anhkheg3
+{
+    class DbSchema : DbContext
+    {
+		public DbSet<Vehicle> Vehicles { get; set; }
+		public DbSet<Purchase> Purchases { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder ob)
+		{
+			ob.UseSqlite("Data Source=purchases.db");
+		}
+	}
+
+	public class Vehicle
+	{
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+		public int Year { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int StartingMileage { get; set; }
+		//TODO: Something for the image here
+
+		public List<Purchase> Purchases { get; set; }
+	}
+
+	public class Purchase
+	{
+		public int ID { get; set; }
+
+		public DateTime Date { get; set; }
+		public decimal Gallons { get; set; }
+		public decimal Cost { get; set; }
+		public decimal Trip { get; set; }
+		public int Odometer { get; set; }
+
+		public Vehicle Vehicle;
+	}
+}
